7051505efadde66cdbfc1fe91cc516a025cbc26a
Dhruv Singh <dhruvsingh7077@gmail.com>
2025-12-04 22:04:52 +0530

    implmnt project

--- src/windows/Finder.jsx
+++ src/windows/Finder.jsx
@@ -5,9 +5,19 @@ import WindowWrapper from "#hoc/WindowWrapper.jsx";
 import useLocationStore from "#store/location.js";
 import clsx from "clsx";
 import { locations } from "../constants/index.js";
+import useWindowStore from "#store/window.js";
 const Finder = () => {
+  const { openWindow } = useWindowStore();
   const { activeLocation, setActiveLocation } = useLocationStore();
 
+  const openItem = (item) => {
+    if (item.fileType === "pdf") return openWindow("resume");
+    if (item.kind === "folder") return setActiveLocation(item);
+    if (["fig", "url"].includes(item.fileType) && item.href)
+      return window.open(item.href, "_blank");
+
+    openWindow(`${item.fileType}${item.kind}`, item);
+  };
   const renderList = (name, items = []) => (
     <div>
       <h3>{name}</h3>
@@ -37,19 +47,22 @@ const Finder = () => {
         <div className="sidebar">
           {renderList("Favourites", Object.values(locations ?? {}))}
 
-          {renderList("Work", locations?.work?.children ?? [])}
+          {renderList("My Projects", locations?.work?.children ?? [])}
         </div>
-      </div>
 
-      <ul className="content">
-        {activeLocation?.children.map((item) => (
-          <li
-            key={item.id}
-            className={item.position}
-            onClick={() => openItem(item)}
-          ></li>
-        ))}
-      </ul>
+        <ul className="content">
+          {activeLocation?.children.map((item) => (
+            <li
+              key={item.id}
+              className={item.position}
+              onClick={() => openItem(item)}
+            >
+              <img src={item.icon} alt={item.name} />
+              <p>{item.name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
