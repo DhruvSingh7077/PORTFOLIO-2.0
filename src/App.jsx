a6555779f5c8b64c3796f71eefc253150a13c713
Dhruv Singh <dhruvsingh7077@gmail.com>
2025-12-07 20:03:18 +0530

    adeeded the photos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import gsap from "gsap";
-import { Navbar, Welcome, Dock } from "#components";
+import { Navbar, Welcome, Dock, Home } from "#components";
 import { Draggable } from "gsap/Draggable";
-import { Terminal } from "#windows/index.js";
+import { Photos, Terminal } from "#windows/index.js";
 import { Safari } from "#windows/index.js";
 import { Resume } from "#windows/index.js";
-import { Finder, Text, Image } from "#windows/index.js";
+import { Finder, Text, Image, Contact } from "#windows/index.js";
 gsap.registerPlugin(Draggable);
 const App = () => {
   return (
@@ -20,6 +20,9 @@ const App = () => {
       <Finder />
       <Text />
       <Image />
+      <Contact />
+      <Home />
+      <Photos />
     </main>
   );
 };
--- src/windows/index.js
+++ src/windows/index.js
@@ -4,4 +4,6 @@ import Resume from "./Resume.jsx";
 import Finder from "./Finder.jsx";
 import Text from "./Text.jsx";
 import Image from "./Image.jsx";
-export { Terminal, Safari, Resume, Finder, Text, Image };
+import Contact from "./Contact.jsx";
+import Photos from "./Photos.jsx";
+export { Terminal, Safari, Resume, Finder, Text, Image, Contact, Photos };
